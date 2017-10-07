372e28afc57fc0e3e48516eefb1eae3c883c4d05
Yogesh Manghnani <yogesh@localhost.localdomain>
2017-10-07 19:05:42 +0530

    added solution to problem 17

---
+++ problem_17.py
@@ -0,0 +1,15 @@
+##much of it is done on a calculator
+##very easy so i didnt code it 
+##this is just for ref 
+
+
+onetonine=36
+tentonineteen=70
+twentytoninetynine=748
+hundredtonineninetynine=20259
+onethousand=11
+
+sumof=onetonine+tentonineteen+twentytoninetynine+hundredtonineninetynine+onethousand
+
+
+print(sumof)
