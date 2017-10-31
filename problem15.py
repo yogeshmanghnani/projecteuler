2092f2938454db054505a3715c5c35cf5f0534c0
yogeshmanghnani <yamanghnani@gmail.com>
2017-10-31 10:17:16 +0530

    Solution to problem 15 completed.

--- problem15.py
+++ problem15.py
@@ -5,13 +5,30 @@
     
     
 def init_dictionary():
+    """This function initializes the dictionary
+    It is returns the intialized dictionary
+    It does so for 20 X 20 grid"""
     lattice_points = {}
-    for row in range(1 , 21):
-        lattice_points[(row , 1)] = 1
-    for coloumn in range(1, 21):
-        lattice_points[(1, coloumn)] = 1
+    for row in range(21):
+        lattice_points[(row , 0)] = 1
+    for coloumn in range(21):
+        lattice_points[(0, coloumn)] = 1
+    lattice_points[(0 , 0)] = 0
         
     return lattice_points
 
 
+def find_number_of_ways(lattice_points, curr_point):
+    if curr_point in lattice_points.keys():
+        return lattice_points[curr_point]
+    else:
+        print(curr_point)
+        z = find_number_of_ways(lattice_points, (curr_point[0]-1, curr_point[1]))+find_number_of_ways(lattice_points, (curr_point[0], curr_point[1]-1))
+        lattice_points[curr_point] = z
+        return z
+
 points = init_dictionary()
+k = find_number_of_ways(points, (20, 20))
+print(k)
+
+
