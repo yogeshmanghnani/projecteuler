f79448aa38236a5f5e46b29ddb1aac970090f067
yogeshmanghnani <yamanghnani@gmail.com>
2017-11-01 17:18:52 +0530

    Solution to problem 4 added

---
+++ problem_4.py
@@ -0,0 +1,13 @@
+largest = 1
+k = 1
+z = "init"
+for i in range (100, 1000):
+    for j in range (100, 1000):
+        k=i*j
+        z=str(k)
+        if z == z[::-1] and k>largest:
+            largest = k
+            
+            
+
+print(largest)
