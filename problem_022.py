52c4c8a56c8fffa6f654f2af60f1e8c18ba6f766
yogeshmanghnani <yamanghnani@gmail.com>
2018-04-12 16:06:11 +0530

    problem 22 solved

---
+++ problem_022.py
@@ -0,0 +1,25 @@
+import string
+
+if __name__ == "__main__":
+
+    # open file and put names in a list
+    file_name = "problem_022.txt"
+    file = open(file_name, "r")
+    all_names = file.read()
+    all_names = all_names.replace('"', '')
+    all_names = all_names.split(',')
+    all_names.sort()
+
+    # make list of alphabets
+    upper_case_alphabets = string.ascii_uppercase
+    upper_case_alphabets = list(upper_case_alphabets)
+
+    total_word_score = 0
+    for name in all_names:
+        curr_name_score = 0
+        for char in name:
+            curr_name_score += upper_case_alphabets.index(char) + 1
+
+        curr_name_score *= (all_names.index(name) + 1)
+        total_word_score += curr_name_score
+    print(total_word_score)
