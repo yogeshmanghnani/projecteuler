c6caecbad6b9bbb8c4da7e77fbd377aa7a36724d
yogeshmanghnani <yamanghnani@gmail.com>
2018-06-27 20:29:50 +0530

    solved problem 009

---
+++ problem_009.py
@@ -0,0 +1,10 @@
+for c in range(1, 1001):
+    for b in range(1, 1001):
+        for a in range(1, 1001):
+            if a<b and b<c:
+                if a**2 + b**2 == c**2 and a+b+c == 1000:
+                    print(a, b, c)
+                    print(a*b*c)
+            else:
+                break
+        
